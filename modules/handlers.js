6c0a834cb63a42981e99c7c3cda56c59eee3f546
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-02-01 09:48:34 +0100

    Add welcom site

--- modules/handlers.js
+++ modules/handlers.js
@@ -1,17 +1,24 @@
+var fs = require('fs');
+
 exports.upload = function (request, response) {
     console.log('Rozpoczynam obsługę żądania upload.');
     response.write('Rozpoczynam upload!');
     response.end();
-}
+};
 
 exports.welcome = function (request, response) {
     console.log('Rozpoczynam obsługę żądania welcome.');
-    response.write('Witaj na stronie startowej!');
-    response.end();
-}
+    fs.readFile('templates/start.html', function (err, html) {
+        response.writeHead(200, {
+            "Content-Type": "text/html; charset=utf-8"
+        });
+        response.write(html);
+        response.end();
+    });
+};
 
 exports.error = function (request, response) {
     console.log('Nie wiem co robić.');
     response.write('404 :(');
     response.end();
-}
+};
