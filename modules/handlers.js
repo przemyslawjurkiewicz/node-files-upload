608def963d5f0a0ac47a27a985e59d8df0f530d0
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-02-07 17:49:30 +0100

    Fix fileName algorithm

--- modules/handlers.js
+++ modules/handlers.js
@@ -1,43 +1,65 @@
 var fs = require('fs');
 var formidable = require('formidable');
+var fileName = '';
 
 exports.upload = function(request, response) {
-    console.log('Rozpoczynam obsługę żądania upload.');
-    var form = new formidable.IncomingForm();
-    form.parse(request, function(err, fields, files) {
-        if (err) throw console.err();
-        fs.renameSync(files.upload.path, "test.png");
-        response.writeHead(200, {'Content-Type': 'text/html'});
-        response.write('received image:<br/>');
-        response.write('<img src="/show" />');
-        response.end();
+  console.log('Rozpoczynam obsługę żądania upload.');
+  var form = new formidable.IncomingForm();
+  //fomidable parse form fields and files
+  form.parse(request, function(err, fields, files) {
+    if (err) throw console.err();
+    //get orginal file extencion
+    var extension = files.upload.name.match(/\..+$/);
+    //if title is empty fileName is orginal name
+    fileName = !fields.title ? files.upload.name : fields.title + extension;
+    //rename file and add img/ to path
+    fs.renameSync(files.upload.path, 'img/' + fileName);
+    fs.readFile('templates/upload.html', function(err, html) {
+      if (err) throw console.err();
+      response.writeHead(200, {
+        'Content-Type': 'text/html; charset=utf-8'
+      });
+      response.write(html);
+      response.end();
     });
-}
-    
-
-exports.show = function (request, response) {
-    fs.readFile('test.png', 'binary', function (err, file) {
-        if (err) throw console.err();
-        response.writeHead(200, {'Content-Type': 'image/png'});
-        response.write(file, 'binary');
-        response.end();
-    });
-}
-
-exports.welcome = function (request, response) {
-    console.log('Rozpoczynam obsługę żądania welcome.');
-    fs.readFile('templates/start.html', function (err, html) {
-        if (err) throw console.err();      
-        response.writeHead(200, {
-            'Content-Type': 'text/html; charset=utf-8'
-        });
-        response.write(html);
-        response.end();
-    });
+  });
 };
 
-exports.error = function (request, response) {
-    console.log('Nie wiem co robić.');
-    response.write('404 :(');
+exports.show = function(request, response) {
+  fs.readFile('img/' + fileName, 'binary', function(err, file) {
+    if (err) throw console.err();
+    response.writeHead(200, { 'Content-Type': 'image/png' });
+    response.write(file, 'binary');
     response.end();
+  });
+};
+
+exports.welcome = function(request, response) {
+  console.log('Rozpoczynam obsługę żądania welcome.');
+  fs.readFile('templates/start.html', function(err, html) {
+    if (err) throw console.err();
+    response.writeHead(200, {
+      'Content-Type': 'text/html; charset=utf-8'
+    });
+    response.write(html);
+    response.end();
+  });
+};
+
+exports.css = function(request, response) {
+  console.log('Wysyłam plik css');
+  fs.readFile('templates/style.css', function(err, css) {
+    if (err) throw console.err();
+    response.writeHead(200, {
+      'Content-Type': 'text/css'
+    });
+    response.write(css);
+    response.end();
+  });
+};
+
+exports.error = function(request, response) {
+  console.log('Nie wiem co robić.');
+  response.write('404 :(');
+  response.end();
 };
--- modules/server.js
+++ modules/server.js
@@ -3,31 +3,34 @@ var colors = require('colors');
 var handlers = require('./handlers');
 
 function start() {
-    function onRequest(request, response) {
-        console.log('Odebrano zapytanie'.green);
-        console.log('Zapytanie' + request.url + ' odebrane');
+  function onRequest(request, response) {
+    console.log('Odebrano zapytanie'.green);
+    console.log('Zapytanie' + request.url + ' odebrane');
 
-        response.writeHead(200, {
-            'Content-Type': 'text/plain; charset=utf-8'
-        });
-        switch (request.url) {
-            case '/':
-            case '/start':
-                handlers.welcome(request, response);
-                break;
-            case '/upload':
-                handlers.upload(request, response);
-                break;
-            case '/show':
-                handlers.show(request, response);
-                break;
-            default:
-                handlers.error(request, response);
-        };
-    };
+    response.writeHead(200, {
+      'Content-Type': 'text/plain; charset=utf-8'
+    });
+    switch (request.url) {
+      case '/':
+      case '/start':
+        handlers.welcome(request, response);
+        break;
+      case '/style.css':
+        handlers.css(request, response);
+        break;
+      case '/upload':
+        handlers.upload(request, response);
+        break;
+      case '/show':
+        handlers.show(request, response);
+        break;
+      default:
+        handlers.error(request, response);
+    }
+  }
 
-    http.createServer(onRequest).listen(9000);
-    console.log('Uruchomiono serwer'.green);
-};
+  http.createServer(onRequest).listen(9000);
+  console.log('Uruchomiono serwer'.green);
+}
 
 exports.start = start;
